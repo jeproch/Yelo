9497f9306afe29309a69cc98d991eb5b129a40c9
jeproch <jprevenant@gmail.com>
2023-07-21 19:20:11 -0700

    Philosophy Page UI

--- chatrooms/chatrooms.js
+++ chatrooms/chatrooms.js
@@ -1,7 +1,18 @@
 //LOCAL
+const philosophyPage = document.getElementById("philosophy-page");
+const menuPage = document.getElementById("menu-page");
+
+document.addEventListener("DOMContentLoaded", function () {
+  menuPage.classList.add("hide");
+  philosophyPage.classList.remove("hide");
+});
 
 const menuBtn = document.getElementById("menu-btn");
 
 menuBtn.addEventListener("click", function () {
   console.log("works");
+});
+menuBtn.addEventListener("click", function () {
+  philosophyPage.classList.add("hide");
+  menuPage.classList.remove("hide");
 });
